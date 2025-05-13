efc85d0e52c46137ed2a24cec2cc2fd98e3de0d9
Yusuke Kamiyamane <y@y14e.com>
2025-05-13 21:54:18 +0900

    20250513

---
+++ load-images.ts
@@ -0,0 +1,9 @@
+export function loadImages(urls: string[]): Promise<HTMLImageElement[]> {
+  async function load(url: string): Promise<HTMLImageElement> {
+    const image = new Image();
+    image.setAttribute('src', url);
+    await image.decode();
+    return image;
+  }
+  return Promise.all(urls.map(url => load(url)));
+}
